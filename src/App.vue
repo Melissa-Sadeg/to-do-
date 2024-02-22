e4ef1a3ee0f82fecb9b707d61faefc413fd3bfdc
melissa sadeg <sadeg@et.esiea.fr>
2024-02-22 16:57:20 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <!-- parti e html  -->
 <template>
-  <form>
-    <input type="text" />
+  <form @submit.prevent="ajouterTodo()">
+    <input v-model="nexTodo" type="text" @input="test" />
     <button>Ajouter</button>
-    <ul>
-      <li>Faire du sport</li>
-      <li>apprendre React</li>
-      <li>Lire un livre</li>
-    </ul>
+    <ol>
+      <li v-for="element in todos" :key="element.id">
+        {{ element.details }}
+        <button @click="deleteTodo(element)">Delete</button>
+      </li>
+    </ol>
   </form>
 </template>
 
 <!-- partie java script -->
-<script>
+<script setup>
 import { ref } from "vue";
 const todos = ref([]);
+//tablaeu qui contient tous les tache ecrit par user
+
+const nexTodo = ref("");
+
+function ajouterTodo() {
+  if (nexTodo.value === "") return; // ne pas ajouter si vide
+  todos.value.push({ id: new Date().getTime(), details: nexTodo.value }); // ajouter en ordre
+  nexTodo.value = ""; //// supprimer la val apres lAjout
+}
+
+function deleteTodo(todo) {
+  todos.value = todos.value.filter((element) => element !== todo);
+}
 </script>
