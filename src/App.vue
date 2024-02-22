d0e63fe000abf6e79356141cec038526ee51a978
melissa sadeg <sadeg@et.esiea.fr>
2024-02-22 17:32:10 +0100

    delete

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <!-- parti e html  -->
 <template>
   <form @submit.prevent="ajouterTodo()">
-    <input v-model="nexTodo" type="text" @input="test" />
+    <input
+      v-model="nexTodo"
+      type="text"
+      @input="test"
+      placeholder="new todo..."
+    />
     <button>Ajouter</button>
     <ol>
+      <p v-if="todos.length === 0">pas de todo !</p>
       <li v-for="element in todos" :key="element.id">
+        <input type="checkbox" @v-model="t.fini" />
         {{ element.details }}
         <button @click="deleteTodo(element)">Delete</button>
       </li>
@@ -14,19 +21,34 @@
 
 <!-- partie java script -->
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 const todos = ref([]);
 //tablaeu qui contient tous les tache ecrit par user
 
 const nexTodo = ref("");
+onMounted(() => {
+  const todosSave = localStorage.getItem("todos");
+  if (todosSave) {
+    todos.value = JSON.parse(todosSave);
+  } else {
+    todos.value = [];
+  }
+});
 
 function ajouterTodo() {
   if (nexTodo.value === "") return; // ne pas ajouter si vide
-  todos.value.push({ id: new Date().getTime(), details: nexTodo.value }); // ajouter en ordre
+  todos.value.push({
+    id: new Date().getTime(),
+    details: nexTodo.value,
+    fini: false,
+  }); // ajouter en ordre
+
   nexTodo.value = ""; //// supprimer la val apres lAjout
+  localStorage.setItem("todos", JSON.stringify(todos.value));
 }
 
 function deleteTodo(todo) {
-  todos.value = todos.value.filter((element) => element !== todo);
+  todos.value = todos.value.filter((element) => element !== todo); // a l'aide du filtrage on supprime
+  localStorage.setItem("todos", JSON.stringify(todos.value));
 }
 </script>
