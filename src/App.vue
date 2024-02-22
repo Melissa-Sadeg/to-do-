8f3ca560b9a2debb808783bb3571094b7f708175
melissa sadeg <sadeg@et.esiea.fr>
2024-02-22 15:51:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- parti e html  -->
+<template>
+  <form>
+    <input type="text" />
+    <button>Ajouter</button>
+    <ul>
+      <li>Faire du sport</li>
+      <li>apprendre React</li>
+      <li>Lire un livre</li>
+    </ul>
+  </form>
+</template>
+
+<!-- partie java script -->
+<script>
+import { ref } from "vue";
+</script>
